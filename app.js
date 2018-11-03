226532d0aee6081f0dde301d3e74a6fb5f3fd6a6
chiyilin <cyl19980205@gmail.com>
2018-11-03 17:07:12 +0800

    11.3 周六修改重复登陆、修改模板消息、对接报名支付

--- app.js
+++ app.js
@@ -1,55 +1,65 @@
 //app.js
 App({
-  onLaunch: function () {
-    // console.log('first')
+  onLaunch: function() {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    if (!wx.getStorageSync('userInfo')) {
-      // 登录
-      wx.login({
-        success: res => {
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          // console.log(res.code)
-          wx.request({
-            url: getApp().globalData.apiurl + 'login/login',
-            method: 'post',
-            data: {
-              code: res.code
-            },
-            success: function (data) {
-              console.log(JSON.parse(data.data.data))
-              getApp().globalData.openid = JSON.parse(data.data.data).openid;
-              // console.log(getApp().globalData.openid)
+    wx.getSetting({
+      success: function(e) {
+        if (!e.authSetting['scope.userInfo']) {
+          // 登录
+          wx.login({
+            success: res => {
+              // 发送 res.code 到后台换取 openId, sessionKey, unionId
+              // console.log(res.code)
               wx.request({
-                url: getApp().globalData.apiurl + 'login/authcheck',
+                url: getApp().globalData.apiurl + 'login/login',
+                method: 'post',
                 data: {
-                  openid: getApp().globalData.openid
+                  code: res.code
                 },
-                success: function (data) {
-                  // console.log(data.data.data)
-                  if (data.data.code == 200) {
-                    if (data.data.data.user_is_lock == 1) {
-                      console.log('power')
-                      wx.setStorageSync('userInfo', data.data.data);
-                      wx.switchTab({
-                        url: '/pages/index/index',
-                      })
-                    } else {
+                success: function(data) {
+                  console.log(JSON.parse(data.data.data))
+                  getApp().globalData.openid = JSON.parse(data.data.data).openid;
+                  // console.log(getApp().globalData.openid)
+                  wx.request({
+                    url: getApp().globalData.apiurl + 'login/authcheck',
+                    data: {
+                      openid: getApp().globalData.openid
+                    },
+                    success: function(data) {
+                      // console.log(data.data.data)
+                      if (data.data.code == 200) {
+                        if (data.data.data.user_is_lock == 1) {
+                          console.log('power')
+                          wx.setStorageSync('userInfo', data.data.data);
+                          wx.switchTab({
+                            url: '/pages/mhzindex/mhzindex',
+                          })
+                          
+                        } else {
 
+                        }
+                        // wx.redirectTo({
+                        //   url: '/pages/Login/Login',
+                        // })
+
+                      }else{
+                        wx.navigateTo({
+                          url: '/pages/Login/againLogin/againLogin',
+                        })
+                      }
                     }
-                    // wx.redirectTo({
-                    //   url: '/pages/Login/Login',
-                    // })
-
-                  }
+                  })
                 }
-              })
+              });
             }
-          });
+          })
+        } else {
+          console.log('授权过')
         }
-      })
+      }
+    })
+    if (!wx.getStorageSync('userInfo')) {
+
     }
   },
   globalData: {
